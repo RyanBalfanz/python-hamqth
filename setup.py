eef78fe11322ff416a93d5c5274dabae76b98276
Ryan Balfanz <662453-ryanbalfanz@users.noreply.gitlab.com>
2020-04-11 23:18:01 -0700

    Prepare for packing and distribution

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+# Configuring setup() using setup.cfg files
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#configuring-setup-using-setup-cfg-files
+setup()
